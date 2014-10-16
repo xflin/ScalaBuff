eb8717009de40c1060be2fe73dcaf4eed91a68f2
Xiaofeng Lin <xflin@xflin-mac15-1tb.local>
2014-10-16 15:25:15 -0700

    changed build version to 1.3.9.2

--- project/ScalaBuffBuild.scala
+++ project/ScalaBuffBuild.scala
@@ -22,7 +22,7 @@ object ScalaBuffBuild extends Build {
 	lazy val buildSettings = Seq(
 		name := "ScalaBuff",
 		organization := "net.sandrogrzicic",
-		version := "1.3.9.1",
+		version := "1.3.9.2",
 		scalaVersion := "2.11.2",
 		logLevel := Level.Info
 	)
